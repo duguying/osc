4159273b5d249a304e5ee755d135d651766c7a18
Rex Lee <duguying2008@gmail.com>
2015-04-02 23:18:07 +0800

    完成发送动态功能

--- login/login.go
+++ login/login.go
@@ -1,11 +1,12 @@
 package login
 
 import (
-	"net/url"
-	"path/filepath"
+	"github.com/duguying/osc/utils"
 	"github.com/gogather/com"
 	"github.com/gogather/com/log"
-	"github.com/duguying/osc/utils"
+	"net/url"
+	"path/filepath"
+	"regexp"
 )
 
 func Login(username string, password string) {
@@ -18,24 +19,23 @@ func Login(username string, password string) {
 	pathUsr := filepath.Join(home, ".osc", "username")
 	com.WriteFile(pathUsr, username)
 
-	
 	http := &utils.Http{}
-	response, err := http.Post("https://www.oschina.net/action/user/hash_login",url.Values{
-		"email" : {username},
-		"pwd" : {password},
-		"save_login" : {"1"},
-		})
+	response, err := http.Post("https://www.oschina.net/action/user/hash_login", url.Values{
+		"email":      {username},
+		"pwd":        {password},
+		"save_login": {"1"},
+	})
 
-	if err!=nil {
+	if err != nil {
 		log.Warnln("请检查网络")
 		return
 	}
 
 	// log.Blueln(response)
 	json, err := com.JsonDecode(response)
-	if err==nil {
+	if err == nil {
 		msg, _ := json.(map[string]interface{})["msg"].(string)
-		
+
 		errorCode, ok := json.(map[string]interface{})["error"].(float64)
 		if ok {
 			log.Redf("error[%d] %s,%s\n", int(errorCode), msg, "请去网页版登录")
@@ -45,19 +45,39 @@ func Login(username string, password string) {
 		failCount, ok := json.(map[string]interface{})["failCount"].(float64)
 		if ok {
 			log.Redln(msg)
-			log.Warnln("你还有", 3 - int(failCount), "次尝试的机会")
-		}else{
+			log.Warnln("你还有", 3-int(failCount), "次尝试的机会")
+		} else {
 			log.Redln("Invalid Response")
 		}
-	}else{
+	} else {
 		log.Greenln("登录成功")
+		getUserCode()
 	}
 }
 
-func storeSess() {
-	
-}
+// get user_code
+func getUserCode() {
+	http := &utils.Http{}
+	response, err := http.Get("https://www.oschina.net")
+	if err != nil {
+		log.Redln("[Error]", err)
+		return
+	}
 
-func getSess() {
-	
+	regex1 := `(^[\d\D]*)(name='user_code' value=')([\d\D][^\/]+)('\/>)([\d\D]*$)`
+	reg := regexp.MustCompile(regex1)
+	userCode := reg.ReplaceAllString(response, "$3")
+
+	regex2 := `(^[\d\D]*)(<input type='hidden' name='user' value=')([\d][^']+)('\/>)([\d\D]*$)`
+	reg = regexp.MustCompile(regex2)
+	userId := reg.ReplaceAllString(response, "$3")
+
+	content, _ := com.JsonEncode(map[string]interface{}{
+		"user":      userId,
+		"user_code": userCode,
+	})
+
+	home := utils.GetHome()
+	pathUserCode := filepath.Join(home, ".osc", "userinfo")
+	com.WriteFile(pathUserCode, content)
 }
--- tweet/tweet.go
+++ tweet/tweet.go
@@ -1,5 +1,77 @@
 package tweet
 
+import (
+	"github.com/duguying/osc/utils"
+	"github.com/gogather/com"
+	"github.com/gogather/com/log"
+	"net/url"
+	"path/filepath"
+)
+
 func Tweet(message string) {
-	
+	// read info
+	var data interface{}
+	var err error
+
+	home := utils.GetHome()
+	pathUserInfo := filepath.Join(home, ".osc", "userinfo")
+	if com.FileExist(pathUserInfo) {
+		json := com.ReadFile(pathUserInfo)
+		data, err = com.JsonDecode(json)
+		if err != nil {
+			log.Redln("[Error]", "Parse userinfo file failed")
+			return
+		}
+	} else {
+		log.Warnln("login first")
+		return
+	}
+
+	jsonData, ok := data.(map[string]interface{})
+	if !ok {
+		log.Redln("[Error]", "illeage data")
+		return
+	}
+
+	userId, ok := jsonData["user"].(string)
+	if !ok {
+		log.Redln("[Error]", "get user id failed")
+		return
+	}
+
+	userCode, ok := jsonData["user_code"].(string)
+	if !ok {
+		log.Redln("[Error]", "get user code failed")
+		return
+	}
+
+	http := &utils.Http{}
+	response, err := http.Post("https://www.oschina.net/action/tweet/pub", url.Values{
+		"user":      {userId},
+		"user_code": {userCode},
+		"msg":       {message},
+	})
+
+	if err != nil {
+		log.Warnln("[Error]", err)
+	}
+
+	tweetResult, err := com.JsonDecode(response)
+	if err != nil {
+		log.Redln("发送失败")
+		return
+	} else {
+		_, ok := tweetResult.(map[string]interface{})["error"].(float64)
+		if ok {
+			log.Warnln(tweetResult.(map[string]interface{})["msg"])
+			return
+		}
+
+		id, ok := tweetResult.(map[string]interface{})["log"].(float64)
+		if ok {
+			log.Greenf("开源中国第[%d]条动态发送成功\n", int64(id))
+			log.Blueln(message)
+		}
+	}
+
 }
